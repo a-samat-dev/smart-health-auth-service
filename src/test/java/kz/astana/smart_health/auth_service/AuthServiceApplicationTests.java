e89e58faccecec7d864a824e8869bcff702774cf
samatabibulla <a.samat.dev@gmail.com>
2025-08-30 19:45:22 +0500

    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/test/java/kz/astana/smart_health/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.astana.smart_health.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
